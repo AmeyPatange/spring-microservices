162f146177e0a6ab02dc5aa3127c9316f0652014
Amey <ameypatange@gmail.com>
2021-07-25 19:35:19 +0530

    Config Server Setup Done

--- spring-cloud-config-server/src/main/java/com/springmicroservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/springmicroservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -3,8 +3,11 @@ package com.springmicroservices.springcloudconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.environment.JdbcEnvironmentRepository;
+import org.springframework.context.annotation.Import;
 
 @EnableConfigServer
+@Import({ JdbcEnvironmentRepository.class })
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
